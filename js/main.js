4c86ba42a84bb2db677ff2232559f25039d0b898
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-10-23 20:05:35 +0200

    Project 22 parte 1.1 fix js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@
  deve comunicare il punteggio, cioè il numero di volte
  che l'utente ha inserito un numero consentito.
  */
+
+// Dichiaro le variabili
+
+// Genera bombe computer
+var numeroBombe = [];
+var computerList = [];
+var bombePc = 16;
+
+while (numeroBombe.lenght < bombePc) {
+    var numeroBombe = getRandomNumber(1, 16);
+    
+    if (! numeroBombe.includes(number)) {
+        numeroBombe.push(number);
+    }
+}
+
+console.log(numeroBombe);
+
+
+// Genera numeri random con range
+function getRandomNumber(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Domanda all'utente
+
