30db399e9df64bcdb0d16aed326d119130ac0085
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-10-26 16:48:06 +0100

    Project 22 parte 1.4 fix js

--- js/main.js
+++ js/main.js
@@ -18,40 +18,102 @@
 
 // Dichiaro le variabili
 
-// Genera bombe computer
-var computerBombe = [];
-var bombePc = 16;
+// Setup
+var numeroMax = 0;
+var possibilità = 0;  // 100 - 16
 
-// per non far duplicare i numeri nell'array, utilizzo un while
-while (computerBombe.length < bombePc) {
-    var number = getRandomNumber(1, 100);
+// Genera bombe computer e num consentiti
+var listaBombe = [];  // numeri vietati (bombe)
+var numeriConsentiti = [];  // numeri consentiti inseriti dall'utente
+var utente = 0;  // scelta numero utente
 
-    if (! computerBombe.includes(number)) {
-        computerBombe.push(number);
+// Scelta livello
+var livello = parseInt(prompt('Inserici la difficoltà da 0 a 2').trim());
+while (livello < 0 || livello > 2 || isNaN(livello)) {
+        utente = parseInt(prompt('Il numero di difficoltà deve essere tra 0 e 2').trim());
+}
+    
+switch (livello) {
+    case 0:
+        numeroMax = 100;
+        possibilità = 84;
+        break;
+    case 1:
+        numeroMax = 80;
+        possibilità = 64;
+        break;
+    case 2:
+        numeroMax = 50;
+        possibilità = 34;
+}
+
+
+// Bombe: 16 numeri random univoci
+while (listaBombe.length < 16) {
+    // ottenere numero random
+    var numeroBomba = numeroRandom(numeroMax);
+
+    // push solo se non già presente
+    if (listaBombe.includes(numeroBomba) === false) {
+        listaBombe.push(numeroBomba);
      }
 }
+// Debug numeri bombe
+console.log('Lista bombe:', listaBombe);
 
-console.log(computerBombe);
 
-
-// Genera numeri random con range
-function getRandomNumber(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+/**
+ * MAIN LOOP
+ * 
+ * Loop principale del gioco
+ */
 
 // Domanda all'utente
-var number = parseInt(prompt('Inserisci un numero compreso tra 1 e 100').trim());
+while ((numeriConsentiti.lenght < possibilità) && (listaBombe.includes(utente) !== true )) {
+     // Scelta utente
+    utente = parseInt(prompt('Inserisci un numero da 1 a ' + numeroMax).trim());
+    while (utente < 1 || utente > numeroMax || isNaN(utente)) {
+        utente = parseInt(prompt('Valore non valido. Inserisci un numero da 1 a ' + numeroMax).trim());
+    }
+    console.log('Scelta utente:', utente);
 
-var counter = 0;
-while (counter === computerBombe.lenght) {
-   
-    number = parseInt(prompt('Numero non presente in lista, inserisci un nuovo numero'));
+    // Controllo numero immesso
+    controlloNumero(utente);
 
-    counter++;
+    // Raffronto possibilità
+    if (numeriConsentiti.length === possibilità) {
+        alert('Hai vinto!');
+    }
 }
 
-console.log('Numero presente in lista, hai perso');
- 
+console.log('---GAME OVER---');
+console.log('Lista numeri validi inseriti:', numeriConsentiti);
+console.log('Tentativi riusciti:', numeriConsentiti.length);
 
 
+/**
+ * FUNCTIONS
+ */
 
+// Generazione numeri random 
+function numeroRandom(max) { 
+    return Math.floor(Math.random() * max) + 1;
+}
+
+// Controllo numero utente 
+function controlloNumero(numero) {
+    // Se n è presente nella lista bombe
+    if (listaBombe.includes(numero) === true) {
+        alert('Hai perso :( Hai provato con successo' +
+            numeriConsentiti.length +
+            ' volte prima di trovare la bomba');
+    }
+    // Se n è gia stato inserito in precedenza
+    else if (numeriConsentiti.includes(numero) === true) {
+        alert('Numero già inserito, inserirne un altro');
+     }
+    // Se n non è stato inserito
+    else if (numeriConsentiti.includes(numero) === false) {
+        numeriConsentiti.push(numero);
+     }
+}
